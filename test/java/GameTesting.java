402350d8304dd619c068a911d5a329d3da076e02
slyohmy <simon.westlin@hotmail.com>
2022-09-02 15:13:19 +0200

    Initial commit

---
+++ test/java/GameTesting.java
@@ -0,0 +1,24 @@
+/*import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameTesting {
+
+    private final Game game = new Game();
+
+    /*
+    @Test
+    public void testTooHigh() {
+        assertEquals(Game.MSG_TOO_HIGH, game.guessingNumberGame(NUMBER + Game.attempts + 1));
+    }
+
+    @Test
+    public void testTooLow() {
+        assertEquals(Game.MSG_TOO_LOW, game.guessingNumberGame(NUMBER - Game.attempts - 1));
+    }
+
+    @Test
+    public void testWin() {
+        assertEquals(Game.MSG_WIN, game.guessingNumberGame());
+    }
+}*/
