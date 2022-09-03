7d9667a22da5c516b0533af637d5a65ef98d0c0c
Niklas Sahlberg <niklas.sahlberg@yh.nackademin.se>
2022-09-03 12:08:01 +0200

    Test finalization with the squad

--- test/java/GameTesting.java
+++ test/java/GameTesting.java
@@ -1,24 +1,55 @@
-/*import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTesting {
 
-    private final Game game = new Game();
+    Game game = new Game();
 
-    /*
+
+
+
+
+
+
+
     @Test
     public void testTooHigh() {
-        assertEquals(Game.MSG_TOO_HIGH, game.guessingNumberGame(NUMBER + Game.attempts + 1));
+
+
+
+       game.setGuess(30);
+       game.setNumber(40);
+       game.setMSG(game.MSG_TOO_LOW);
+
+
+
+
+
+        assertEquals(game.getMSG() + game.getGuess(), "The number is greater than " + game.getGuess());
     }
+
 
     @Test
     public void testTooLow() {
-        assertEquals(Game.MSG_TOO_LOW, game.guessingNumberGame(NUMBER - Game.attempts - 1));
+    game.setGuess(99);
+    game.setNumber(50);
+    game.setMSG(game.MSG_TOO_HIGH);
+        assertEquals(game.getMSG() + game.getGuess(), "The number is less than " + game.getGuess());
     }
 
-    @Test
+   @Test
     public void testWin() {
-        assertEquals(Game.MSG_WIN, game.guessingNumberGame());
+        game.setGuess(69);
+        game.setNumber(69);
+        game.setMSG(game.MSG_WIN);
+        assertEquals(game.getMSG(), "Congratulations! You guessed my number!");
+        assertEquals(game.getGuess(), game.getNumber());
+
     }
-}*/
+
+
+}
+
+
+
