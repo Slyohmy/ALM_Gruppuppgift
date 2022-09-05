4f544b62533d3fac245cc3cfd98d8336ac6f0cc1
Ramtin00 <48185328+Ramtin00@users.noreply.github.com>
2022-09-05 15:54:41 +0200

    Ramtin branch (#4)
    * If not sure where to start the program.
    
    * If not sure where to start the program.
    
    * If not sure where to start the program.
    
    * Test finalization with the squad
    
    * Added Maven Framework, classes, tests
    
    * Added Maven Framework, classes, tests
    
    Co-authored-by: Niklas Sahlberg <niklas.sahlberg@yh.nackademin.se>

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,18 +2,56 @@ import java.util.Scanner;
 
 public class Game {
 
-    public static final Scanner scanner = new Scanner(System.in);
-    public static final int number = 1 + (int)(100 * Math.random());
-    public static final int attempts = 5;
-    public static int i, guess;
+    public  final Scanner scanner = new Scanner(System.in);
 
-    public static final String MSG_TOO_HIGH = "The number is less than ";
-    public static final String MSG_TOO_LOW = "The number is greater than ";
-    public static final String MSG_GAMEOVER = "Game over! The number was ";
-    public static final String MSG_WIN = "Congratulations! You guessed my number!";
+    public  int number = 1 + (int)(100 * Math.random());
+    public  final int attempts = 5;
+    public  int i;
+
+
+
+
+    public  int guess;
+
+    public  final String MSG_TOO_HIGH = "The number is less than ";
+    public  final String MSG_TOO_LOW = "The number is greater than ";
+    public  final String MSG_GAMEOVER = "Game over! The number was ";
+    public  final String MSG_WIN = "Congratulations! You guessed my number!";
+
+
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getMSG() {
+        return MSG;
+    }
+
+    public void setMSG(String MSG) {
+        this.MSG = MSG;
+    }
+
+    public  String MSG ="";
+
+    //starts the game here.
+    public  void guessingNumberGame() {
 
     //Start game over here
     public static void guessingNumberGame() {
+
         System.out.println("A random number is chosen between 1-100. Pick the right number within 5 attempts!");
 
         for (i = 0; i < attempts; i++) {
@@ -23,16 +61,19 @@ public class Game {
             guess = scanner.nextInt();
 
             if (number == guess) {
-                System.out.println(MSG_WIN);
+                setMSG(MSG_WIN);
+                System.out.println(MSG);
                 break;
             }
 
             else if (number > guess && i != attempts - 1) {
-                System.out.println(MSG_TOO_LOW + guess);
+                setMSG(MSG_TOO_LOW);
+                System.out.println(MSG + guess);
             }
 
             else if (number < guess && i != attempts - 1) {
-                System.out.println(MSG_TOO_HIGH + guess);
+                setMSG(MSG_TOO_HIGH);
+                System.out.println(MSG + guess);
             }
         }
 
@@ -40,4 +81,5 @@ public class Game {
             System.out.println(MSG_GAMEOVER + number);
         }
     }
+
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-
-    public static void main(String[] args) {
-        Game.guessingNumberGame();
+    public static void main(String arg[]){
+        new GuessingGame();
     }
 }
